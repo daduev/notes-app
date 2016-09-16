e32240739386649833612ee8815a66e09fc0dc54
Adam <daduev.ad@gmail.com>
2016-09-16 21:18:56 +0300

    commit

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+const React = require("react");
+
+const App = React.createClass({
+    render: function(){
+        return (
+            <h1>App</h1>
+        );
+    }
+});
+
+module.exports = App;
